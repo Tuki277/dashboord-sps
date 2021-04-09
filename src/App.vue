9c326db2974db9fa1b0a66b780bb3dff06535ae8
Tran Hoang Huy <huybeos2707@gmail.com>
2021-04-09 17:04:51 +0700

    add change role

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,33 @@
 </template>
 
 <script>
+
+import io from 'socket.io-client'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      socket: io("http://localhost:3003")
+    }
+  },
+  created () {
+    this.socket.on('update', data => {
+      if (data.id === localStorage.getItem('id')) {
+        if (data.message === 'updated') {
+          this.logout()
+        }
+      }
+    })
+  },
+  methods: {
+    logout () {
+      alert("dang nhap lai de thay doi phan quyen")
+      localStorage.removeItem('id')
+      localStorage.removeItem('Token')
+      this.$router.push('/pages/login')
+    }
+  }
 }
 </script>
 
